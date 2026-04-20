f8ae9d30ec31cfb1de48038d17934515bdb66956
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2026-04-20 13:08:19 +0530

    chore: update conversation sidebar interactions (#13988)

--- app/javascript/dashboard/components-next/sidebar/Sidebar.vue
+++ app/javascript/dashboard/components-next/sidebar/Sidebar.vue
@@ -10,8 +10,6 @@ import { useSidebarKeyboardShortcuts } from './useSidebarKeyboardShortcuts';
 import { vOnClickOutside } from '@vueuse/components';
 import { FEATURE_FLAGS } from 'dashboard/featureFlags';
 import { useWindowSize, useEventListener } from '@vueuse/core';
-import { emitter } from 'shared/helpers/mitt';
-import { BUS_EVENTS } from 'shared/constants/busEvents';
 
 import Button from 'dashboard/components-next/button/Button.vue';
 import SidebarGroup from './SidebarGroup.vue';
@@ -181,15 +179,6 @@ const sortedInboxes = computed(() =>
 const closeMobileSidebar = () => {
   if (!props.isMobileSidebarOpen) return;
   emit('closeMobileSidebar');
-};
-
-const onComposeOpen = toggleFn => {
-  toggleFn();
-  emitter.emit(BUS_EVENTS.NEW_CONVERSATION_MODAL, true);
-};
-
-const onComposeClose = () => {
-  emitter.emit(BUS_EVENTS.NEW_CONVERSATION_MODAL, false);
 };
 
 const newReportRoutes = () => [
@@ -733,7 +722,13 @@ const menuItems = computed(() => {
   <aside
     v-on-click-outside="[
       closeMobileSidebar,
-      { ignore: ['#mobile-sidebar-launcher'] },
+      {
+        ignore: [
+          '#mobile-sidebar-launcher',
+          '[data-popover-content]',
+          '[data-popover-backdrop]',
+        ],
+      },
     ]"
     class="bg-n-background flex flex-col text-sm pb-px fixed top-0 ltr:left-0 rtl:right-0 h-full z-40 w-[200px] md:w-auto md:relative md:flex-shrink-0 md:ltr:translate-x-0 md:rtl:translate-x-0 ltr:border-r rtl:border-l border-n-weak"
     :class="[
@@ -823,8 +818,8 @@ const menuItems = computed(() => {
               />
             </template>
           </ComposeInternalChat>
-          <ComposeConversation align-position="right" @close="onComposeClose">
-            <template #trigger="{ toggle, isOpen }">
+          <ComposeConversation align="start">
+            <template #trigger="{ isOpen }">
               <Button
                 icon="i-lucide-pen-line"
                 color="slate"
@@ -833,10 +828,9 @@ const menuItems = computed(() => {
                 :class="[
                   isEffectivelyCollapsed
                     ? '!size-8 !outline-n-weak !text-n-slate-11'
-                    : '!h-7 !outline-n-weak !text-n-slate-11',
+                  : '!h-7 !outline-n-weak !text-n-slate-11',
                   { '!bg-n-alpha-2 dark:!bg-n-slate-9/30': isOpen },
                 ]"
-                @click="onComposeOpen(toggle)"
               />
             </template>
           </ComposeConversation>
@@ -902,5 +896,4 @@ const menuItems = computed(() => {
     </div>
   </aside>
 </template>
-
 
