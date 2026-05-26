1d2d0208f4baa6b21bca009ffcbfa7f10404dabd
Shivam Mishra <scm.mymail@gmail.com>
2026-05-26 17:37:27 +0530

    feat(onboarding): allow full color channel icons (#14570)

--- app/javascript/dashboard/components-next/icon/ChannelIcon.vue
+++ app/javascript/dashboard/components-next/icon/ChannelIcon.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { toRef } from 'vue';
-import { useChannelIcon } from './provider';
+import { computed, toRef } from 'vue';
+import { useChannelIcon, useChannelBrandIcon } from './provider';
 import Icon from 'next/icon/Icon.vue';
 
 const props = defineProps({
@@ -8,11 +8,25 @@ const props = defineProps({
     type: Object,
     required: true,
   },
+  useBrandIcon: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-const channelIcon = useChannelIcon(toRef(props, 'inbox'));
+defineOptions({ inheritAttrs: false });
+
+const inboxRef = toRef(props, 'inbox');
+const channelIcon = useChannelIcon(inboxRef);
+const brandIcon = useChannelBrandIcon(inboxRef);
+
+const icon = computed(() =>
+  props.useBrandIcon && brandIcon.value ? brandIcon.value : channelIcon.value
+);
 </script>
 
 <template>
-  <Icon :icon="channelIcon" />
+  <span class="relative inline-flex" v-bind="$attrs">
+    <Icon :icon="icon" class="size-full" />
+  </span>
 </template>
