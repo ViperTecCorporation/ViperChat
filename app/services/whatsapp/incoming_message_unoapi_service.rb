0d064420f3d278989a309859fe9c4012ff543287
Caitano28 <caitano28@example.com>
2026-07-27 18:07:28 -0400

    fix(whatsapp): convert incoming opus audio to mp3

--- app/services/whatsapp/incoming_message_unoapi_service.rb
+++ app/services/whatsapp/incoming_message_unoapi_service.rb
@@ -1,6 +1,13 @@
 class Whatsapp::IncomingMessageUnoapiService < Whatsapp::IncomingMessageWhatsappCloudService
   private
 
+  def download_attachment_file(attachment_payload)
+    downloaded_file = super
+    return if downloaded_file.blank?
+
+    Whatsapp::Unoapi::AudioTranscoder.new(downloaded_file).perform
+  end
+
   def processed_params
     @processed_params ||= super.presence || params
   end
